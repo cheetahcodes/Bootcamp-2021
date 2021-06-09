e2ac1256bf96fbd438d34e2fe8af32aacf34caae
Cheetah Code Academy <64037451+cheetahcodes@users.noreply.github.com>
2021-06-09 23:07:51 +0100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,34 @@
+// objects
+    const user = {
+      name: 'Chris',
+      username: 'chrisoncode' 
+    };
+
+    // arrays 
+    const users = ['chris', 'nick', 'holly'];
+    const luckyNumbers = [1, 43, 54, 132];
+    const whatever = ['chris', 1, 'holly'];
+    
+    const superUsers = [
+      { name: 'chris' },
+      { name: 'nick' },
+      { name: 'holly' }
+    ];
+
+    console.log(superUsers[2].name);
+
+    // declaring variables
+    var thing        = 'first';
+
+    const otherThing = {
+      name: 'chris'  
+    };
+    let newThing     = 'third';
+
+    otherThing.name = 'holly';
+
+    // ======================
+    let myVariable;
+
+
+    myVariable = 'something';
